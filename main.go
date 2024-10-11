e7c227abe34df5d137e101d4b216488fc5d63287
Azraar Azward <mazraara@gmail.com>
2024-10-11 21:05:51 +0200

    Add possibilities to have string truncation

--- main.go
+++ main.go
@@ -3,11 +3,24 @@ package main
 import (
     "notes-app/controllers"
     "github.com/gin-gonic/gin"
+    "html/template"
 )
+
+func truncate(s string, length int) string {
+    if len(s) <= length {
+        return s
+    }
+    return s[:length] + "..."
+}
 
 func main() {
     r := gin.Default()
-    r.LoadHTMLGlob("templates/*")
+
+    templ := template.Must(template.New("").Funcs(template.FuncMap{
+        "truncate": truncate,
+    }).ParseGlob("templates/*"))
+
+    r.SetHTMLTemplate(templ)
 
     r.GET("/", controllers.ListNotes)
     r.GET("/add", controllers.ShowAddNote)
